c1397d8386a4a294feea8852f96086fcd3b294f4
Joan R <jrbellido@gmail.com>
2020-05-05 23:59:20 +0200

    refactor

--- src/main.rs
+++ src/main.rs
@@ -4,17 +4,21 @@ struct Employee {
     age: u8
 }
 
-fn employee_schedule(day_of_week: u8) -> Vec<Employee> {
-    let employees = vec![
+fn get_employees() -> Vec<Employee>{
+    let employee_list = vec![
         Employee{name: String::from("Max"), age: 17},
         Employee{name: String::from("Sepp"), age: 18},
         Employee{name: String::from("Nina"), age: 15},
         Employee{name: String::from("Mike"), age: 51}
     ];
 
+    employee_list
+}
+
+fn employee_schedule(day_of_week: u8) -> Vec<Employee> {
     let mut result = Vec::new();
 
-    for employee in employees {
+    for employee in get_employees() {
         if day_of_week == 6 {
             if employee.age >= 18 {
                 result.push(employee)
@@ -29,20 +33,19 @@ fn employee_schedule(day_of_week: u8) -> Vec<Employee> {
 
 #[test]
 fn employees_on_sunday_must_be_2() {
-    let result = employee_schedule(6);
+    let result = employee_schedule( 6);
     assert!(result.len() == 2);
 }
 
 #[test]
 fn first_employee_on_sunday_must_be_sepp() {
-    let result = employee_schedule(6);
+    let result = employee_schedule( 6);
     assert!(result[0].name == "Sepp")
 }
 
 #[test]
 fn first_employee_on_monday_must_be_max() {
-    let result = employee_schedule(0);
+    let result = employee_schedule( 0);
     assert!(result[0].name == "Max")
 }
-
 
