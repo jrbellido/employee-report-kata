0376da65baca1c6ac3b2aa916cf52d99f5907389
Joan R <jrbellido@gmail.com>
2020-05-05 23:33:02 +0200

    requirement #0: employees younger than 18 years are not allowed to work on Sunday

---
+++ src/main.rs
@@ -0,0 +1,48 @@
+#[derive(Debug)]
+struct Employee {
+    name: String,
+    age: u8
+}
+
+fn employee_schedule(day_of_week: u8) -> Vec<Employee> {
+    let employees = vec![
+        Employee{name: String::from("Max"), age: 17},
+        Employee{name: String::from("Sepp"), age: 18},
+        Employee{name: String::from("Nina"), age: 15},
+        Employee{name: String::from("Mike"), age: 51}
+    ];
+
+    let mut result = Vec::new();
+
+    for employee in employees {
+        if day_of_week == 6 {
+            if employee.age >= 18 {
+                result.push(employee)
+            }
+        } else {
+            result.push(employee)
+        }
+    }
+
+    return result;
+}
+
+#[test]
+fn employees_on_sunday_must_be_2() {
+    let result = employee_schedule(6);
+    assert!(result.len() == 2);
+}
+
+#[test]
+fn first_employee_on_sunday_must_be_sepp() {
+    let result = employee_schedule(6);
+    assert!(result[0].name == "Sepp")
+}
+
+#[test]
+fn first_employee_on_monday_must_be_max() {
+    let result = employee_schedule(0);
+    assert!(result[0].name == "Max")
+}
+
+
