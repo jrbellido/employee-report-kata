0f12e41483d36ef19f5b48c05a54d5c63a8f4bac
Joan R <jrbellido@gmail.com>
2020-05-06 13:13:57 +0200

    requirement 1: list of employees older than 18

--- src/main.rs
+++ src/main.rs
@@ -4,21 +4,10 @@ struct Employee {
     age: u8
 }
 
-fn get_employees() -> Vec<Employee>{
-    let employee_list = vec![
-        Employee{name: String::from("Max"), age: 17},
-        Employee{name: String::from("Sepp"), age: 18},
-        Employee{name: String::from("Nina"), age: 15},
-        Employee{name: String::from("Mike"), age: 51}
-    ];
-
-    employee_list
-}
-
-fn employee_schedule(day_of_week: u8) -> Vec<Employee> {
+fn employee_schedule(employees: Vec<Employee> ,day_of_week: u8) -> Vec<Employee> {
     let mut result = Vec::new();
 
-    for employee in get_employees() {
+    for employee in employees {
         if day_of_week == 6 {
             if employee.age >= 18 {
                 result.push(employee)
@@ -31,21 +20,45 @@ fn employee_schedule(day_of_week: u8) -> Vec<Employee> {
     return result;
 }
 
-#[test]
-fn employees_on_sunday_must_be_2() {
-    let result = employee_schedule( 6);
-    assert!(result.len() == 2);
+fn employees_older_than_18(employees: Vec<Employee>) -> Vec<Employee> {
+    let mut result = Vec::new();
+
+    for employee in employees {
+        if employee.age >= 18 {
+            result.push(employee)
+        }
+    }
+
+    return result;
 }
 
 #[test]
-fn first_employee_on_sunday_must_be_sepp() {
-    let result = employee_schedule( 6);
-    assert!(result[0].name == "Sepp")
+fn employees_on_sunday_must_be_older_than_18() {
+    let employees = vec![
+        Employee{name: String::from("Max"), age: 17},
+        Employee{name: String::from("Sam"), age: 18}
+    ];
+    let result = employee_schedule(employees, 6);
+    assert_eq!(result.len(), 1)
 }
 
 #[test]
-fn first_employee_on_monday_must_be_max() {
-    let result = employee_schedule( 0);
-    assert!(result[0].name == "Max")
+fn employees_on_monday_can_be_anyone() {
+    let employees = vec![
+        Employee{name: String::from("Max"), age: 17},
+        Employee{name: String::from("Sam"), age: 18}
+    ];
+    let result = employee_schedule(employees, 0);
+    assert_eq!(result[0].name, "Max")
+}
+
+#[test]
+fn get_only_18_employees() {
+    let employees = vec![
+        Employee{name: String::from("Max"), age: 17},
+        Employee{name: String::from("Sam"), age: 18}
+    ];
+    let result = employees_older_than_18(employees);
+    assert_eq!(result.len(), 1)
 }
 
