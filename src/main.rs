9b14e5a1c85c91908ea7c7f395122b0c24b10235
Joan R <jrbellido@gmail.com>
2020-05-06 22:22:17 +0200

    requirement 4: As shop owner I want the employees to be sorted by their names descending instead of ascending

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ struct Employee {
     age: u8
 }
 
+const SORT_ASC: u8 =  1;
+const SORT_DESC: u8 = 2;
+
 fn employee_schedule(employees: Vec<Employee> ,day_of_week: u8) -> Vec<Employee> {
     if day_of_week == 6 {
         return employees_older_than(employees, 18);
@@ -13,6 +16,10 @@ fn employee_schedule(employees: Vec<Employee> ,day_of_week: u8) -> Vec<Employee>
 }
 
 fn employees_older_than(employees: Vec<Employee>, age: u8) -> Vec<Employee> {
+    return employees_older_than_sorted(employees, age, SORT_ASC);
+}
+
+fn employees_older_than_sorted(employees: Vec<Employee>, age: u8, sort: u8) -> Vec<Employee> {
     let mut result = Vec::new();
     for employee in employees {
         if employee.age >= age {
@@ -21,6 +28,9 @@ fn employees_older_than(employees: Vec<Employee>, age: u8) -> Vec<Employee> {
         }
     }
     result.sort_by(|a, b| a.name.cmp(&b.name));
+    if sort == SORT_DESC {
+        result.reverse();
+    }
     return result;
 }
 
@@ -28,7 +38,6 @@ fn capitalize_name(mut employee: Employee) -> Employee {
     let mut capitalized = String::new();
     let mut word_start = true;
     for c in employee.name.chars() {
-        println!("char is {} word_start={}", c, word_start);
         match c {
             ' ' => {
                 capitalized.push(c);
@@ -89,7 +98,7 @@ fn get_employees_sorted() {
         Employee{name: create_name("Sam"), age: 18},
         Employee{name: create_name("Max"), age: 18}
     ];
-    let result = employees_older_than(employees, 18);
+    let result = employees_older_than_sorted(employees, 18, SORT_ASC);
     assert_eq!(result.len(), 2);
     assert_eq!(result[0].name, "Max");
     assert_eq!(result[1].name, "Sam")
@@ -102,5 +111,15 @@ fn get_employees_capitalized() {
     ];
     let result = employees_older_than(employees, 18);
     assert_eq!(result[0].name, "John Doe Aa");
+}
+
+#[test]
+fn get_employees_sorted_desc() {
+    let employees = vec![
+        Employee{name: create_name("Max"), age: 18},
+        Employee{name: create_name("Sam"), age: 18}
+    ];
+    let result = employees_older_than_sorted(employees, 18, SORT_DESC);
+    assert_eq!(result[0].name, "Sam");
 }
 
